7681a00c55dbb6e8d0e470b4021972f40454b7e9
MaxProvin <maxprovin01@gmail.com>
2017-01-02 20:08:07 +0000

    set up webhooks for fb messenger

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000');
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === process.env.FB_WH_TOK) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }
+});
+
+app.listen(8000, function () {
+  console.log('Listening on port 8000');
 });
